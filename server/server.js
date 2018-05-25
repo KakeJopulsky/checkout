06fc9623cec555a106656e409a6b6c78b020e8ed
Jake Kopulsky <jakekopulsky@gmail.com>
2018-05-24 19:50:38 -0700

    Implement insert into payment table

--- server/server.js
+++ server/server.js
@@ -14,20 +14,18 @@ const jsonParser = bodyParser.json()
 */
 // Insert new record into users
 app.post('/insert/account', ({body: { name, email, password }}, res) => {
-  async function newAccount(name, email, password) {
-    await client.query(`INSERT INTO users (name, email, password)
-            VALUES ($1, $2, $3);`,[name, email, password], (err, res) => {
-            if (err) return console.log(err);
-          });
-    }
-    res.sendStatus(200);
+  client.query(`INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3);`,[name, email, password], (err, res) => {
+    if (err) return console.log(err);
+  });
+  res.sendStatus(200);
 });
 
 // Insert new record into shipping
 app.post('/insert/shipping', (
   {body: { email, primary_address = null, secondary_address = null,
   city, state, zip, phoneNumber }}, res) => {
-    
+
     client.query(`INSERT INTO shipping (email, primary_address, secondary_address, city, state, shipping_zip, phone)
       VALUES ($1, $2, $3, $4, $5, $6, $7);`,
       [email, primary_address, secondary_address, city, state, zip, phoneNumber], (err, res) => {
@@ -37,14 +35,12 @@ app.post('/insert/shipping', (
 });
 
 // Insert new record into payment
-app.post('/insert/payment', ({body: { name, email, password }}, res) => {
-  async function newAccount(name, email, password) {
-    await client.query(`INSERT INTO users (name, email, password)
-            VALUES ($1, $2, $3);`,[name, email, password], (err, res) => {
-            if (err) return console.log(err);
-          });
-    }
-    res.sendStatus(200);
+app.post('/insert/payment', ({body: { email, creditCardNumber, expDate, cvv, billingZip }}, res) => {
+  client.query(`INSERT INTO payment (email, credit_card_number, exp_date, cvv, billing_zip)
+    VALUES ($1, $2, $3, $4, $5);`,[email, creditCardNumber, expDate, cvv, billingZip], (err, res) => {
+    if (err) return console.log(err);
+  });
+  res.sendStatus(200);
 });
 
 app.get('/', (req, res) => res.send('Hello World!'));
